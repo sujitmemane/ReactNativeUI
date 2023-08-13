232ab6702302a2c6ed56f0aac5b707f463f46e88
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-08-13 10:21:03 +0530

    initial-commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+import FlatCards from './components/FlatCards';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <Text>App</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
